b04a6723158681a046db4d8fbd46c4361a554a2d
Zenas Chen <czyczk@qq.com>
2021-12-23 00:32:58 +0800

    Passed a simple Polkadot API test. A great start

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 const restify = require('restify');
+const { ApiPromise, WsProvider } = require('@polkadot/api');
 
 const server = restify.createServer();
 server.get('/hello/:name', respond);
+server.get('/genesis-hash', testNodeConnection);
 
 function respond(req, res, next) {
 	res.send('hello, ' + req.params.name);
+	next();
+}
+
+async function testNodeConnection(req, res, next) {
+	const wsProvider = new WsProvider('ws://localhost:9944');
+	const api = new ApiPromise({ provider: wsProvider });
+	await api.isReady;
+	const genesisHash = api.genesisHash.toHex();
+	res.send(genesisHash);
 	next();
 }
 
