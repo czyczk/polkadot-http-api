6c3a5b533eab4c579ffbdb9db326182c0b026057
Zenas Chen <czyczk@qq.com>
2021-12-23 11:29:40 +0800

    Turned the project into a Typescript project.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-const restify = require('restify');
-const { ApiPromise, WsProvider } = require('@polkadot/api');
+import restify, { Next, Request, Response } from 'restify';
+import { ApiPromise, WsProvider } from '@polkadot/api';
 
 const server = restify.createServer();
 server.get('/hello/:name', respond);
 server.get('/genesis-hash', testNodeConnection);
 
-function respond(req, res, next) {
+function respond(req: Request, res: Response, next: Next) {
 	res.send('hello, ' + req.params.name);
 	next();
 }
 
-async function testNodeConnection(req, res, next) {
+async function testNodeConnection(req: Request, res: Response, next: Next) {
 	const wsProvider = new WsProvider('ws://localhost:9944');
 	const api = new ApiPromise({ provider: wsProvider });
 	await api.isReady;
