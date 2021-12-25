a93a0347e09d82d99b699cc39d1a8b645aaf2459
Zenas Chen <czyczk@qq.com>
2021-12-25 21:44:40 +0800

    QueryController extras & RPCController trivial tweaks

--- src/controller/query-controller.ts
+++ src/controller/query-controller.ts
@@ -3,15 +3,17 @@ import errs from 'restify-errors';
 import { Endpoint, IGroupableController } from './model';
 import HTTPMethod from 'http-method-enum';
 import { ApiPromise } from '@polkadot/api';
+import { Codec } from '@polkadot/types/types';
+import { u64 } from '@polkadot/types/primitive';
 
 export class QueryController implements IGroupableController {
 	constructor(private readonly _api: ApiPromise) { }
 
-	handleGetNow = async (req: Request, res: Response, next: Next) => {
+	handleGetStakingValidators = async (req: Request, res: Response, next: Next) => {
 		try {
 			await this._api.isReady;
-			const now = await this._api.query.timestamp.now();
-			res.send(200, now.toJSON());
+			const validatorKeys = await this._api.query.staking.validators.keys();
+			res.send(200, validatorKeys);
 			next();
 		} catch (err) {
 			console.error(err);
@@ -22,13 +24,41 @@ export class QueryController implements IGroupableController {
 	handleGetSystemAccount = async (req: Request, res: Response, next: Next) => {
 		try {
 			const addr = req.params.addr;
-			if (addr === '') {
+			if (!addr) {
 				next(new errs.BadRequestError('Param `addr` not specified.'));
 				return;
 			}
 
-			const result = await this._api.query.system.account(addr);
+			const blockHash = req.query.at;
+
+			await this._api.isReady;
+			let result: Codec;
+			if (!blockHash) {
+				result = await this._api.query.system.account(addr);
+			} else {
+				result = await (await this._api.at(blockHash)).query.system.account(addr);
+			}
+
 			res.send(200, result);
+			next();
+		} catch (err) {
+			console.error(err);
+			next(err);
+		}
+	};
+
+	handleGetNow = async (req: Request, res: Response, next: Next) => {
+		try {
+			const blockHash = req.query.at;
+
+			await this._api.isReady;
+			let now: u64;
+			if (!blockHash) {
+				now = await this._api.query.timestamp.now();
+			} else {
+				now = await (await this._api.at(blockHash)).query.timestamp.now();
+			}
+			res.send(200, now.toJSON());
 			next();
 		} catch (err) {
 			console.error(err);
@@ -38,7 +68,8 @@ export class QueryController implements IGroupableController {
 
 	prefix = '/api/query';
 	endpoints = [
-		new Endpoint(HTTPMethod.GET, '/timestamp/now', [this.handleGetNow]),
+		new Endpoint(HTTPMethod.GET, '/staking/validators/keys', [this.handleGetStakingValidators]),
 		new Endpoint(HTTPMethod.GET, '/system/account/:addr', [this.handleGetSystemAccount]),
+		new Endpoint(HTTPMethod.GET, '/timestamp/now', [this.handleGetNow]),
 	];
 }
--- src/controller/rpc-controller.ts
+++ src/controller/rpc-controller.ts
@@ -6,18 +6,6 @@ import { Endpoint, IGroupableController } from './model';
 export class RPCController implements IGroupableController {
 	constructor(private readonly _api: ApiPromise) { }
 
-	handleGetSystemChain = async (req: Request, res: Response, next: Next) => {
-		try {
-			await this._api.isReady;
-			const chain = await this._api.rpc.system.chain();
-			res.send(200, chain);
-			next();
-		} catch (err) {
-			console.error(err);
-			next(err);
-		}
-	};
-
 	handleGetChainHeader = async (req: Request, res: Response, next: Next) => {
 		try {
 			await this._api.isReady;
@@ -30,9 +18,21 @@ export class RPCController implements IGroupableController {
 		}
 	};
 
+	handleGetSystemChain = async (req: Request, res: Response, next: Next) => {
+		try {
+			await this._api.isReady;
+			const chain = await this._api.rpc.system.chain();
+			res.send(200, chain);
+			next();
+		} catch (err) {
+			console.error(err);
+			next(err);
+		}
+	};
+
 	prefix = '/api/rpc';
 	endpoints = [
-		new Endpoint(HTTPMethod.GET, '/system/chain', [this.handleGetSystemChain]),
 		new Endpoint(HTTPMethod.GET, '/chain/header', [this.handleGetChainHeader]),
+		new Endpoint(HTTPMethod.GET, '/system/chain', [this.handleGetSystemChain]),
 	];
 }
